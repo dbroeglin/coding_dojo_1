94c80621df299809a7cc4a36fa865c97accbee09
Dominique Broeglin <dominique.broeglin@gmail.com>
2013-06-18 23:33:52 +0200

    Adapted for running with node

--- specs/gilded_rose_spec.js
+++ specs/gilded_rose_spec.js
@@ -1,3 +1,8 @@
+var GildedRose = require('../lib/gilded_rose');
+var items = GildedRose.items, 
+  update_quality = GildedRose.update_quality;
+
+
 describe("Gilded Rose", function() {
 
   it("should do something", function() {
